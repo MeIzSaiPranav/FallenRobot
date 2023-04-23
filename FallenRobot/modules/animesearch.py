af0788fc294195c404819d84a593592b5cbf0b03
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2023-04-23 17:22:38 +0530

    Update animesearch.py
    Signed-off-by: MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>

--- FallenRobot/modules/animesearch.py
+++ FallenRobot/modules/animesearch.py
@@ -1,11 +1,17 @@
 import requests
 
-def animeSearch(bot, update):
+def anime_search(bot, update):
     url = "https://api.jikan.moe/v4/search/anime?q="
     search_query = update.message.text.split()[1:]
     search_query = "+".join(search_query)
-    res = requests.get(url+search_query).json()
-    anime_list = res["data"]
-    for anime in anime_list:
-        message = "{}\n\nSynopsis: {}\n\nScore: {}\n\n".format(anime["attributes"]["canonicalTitle"], anime["attributes"]["synopsis"], anime["attributes"]["averageRating"])
-        bot.send_message(chat_id=update.message.chat_id, text=message)
+    if search_query:
+        res = requests.get(url+search_query)
+        if res.status_code == 200:
+            anime_list = res.json()["data"]
+            for anime in anime_list:
+                message = "{}\n\nSynopsis: {}\n\nScore: {}\n\n".format(anime["attributes"]["canonicalTitle"], anime["attributes"]["synopsis"], anime["attributes"]["averageRating"])
+                bot.send_message(chat_id=update.message.chat_id, text=message)
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text="Error: Failed to retrieve data from API")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text="Error: Please enter a search query")
